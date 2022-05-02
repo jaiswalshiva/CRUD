4e913e1e5731dbb5a68dd1193cdfa94685e97540
Ritik Panday <panday.ritik@tftus.com>
2022-05-02 10:17:05 +0530

    final

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,9 @@ const bcrypt = require('bcrypt');
 const Model = require('../models/usersModel');
 const emailvalidator = require('email-validator');
 const sendEmail = require('../services/email');
-// const Model = require('../models/expenseModel');
+const expense = require('../models/expenseModel');
+const tokenModel = require('../models/tokenModel');
+
 
 // it is use the create or add a new data in the Databse
 module.exports.create = async function (req, res, next) {
@@ -85,6 +87,73 @@ module.exports.delete = async function (req, res, next) {
     const id = req.params.id;
     const data = await Model.findByIdAndDelete(id);
     res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+module.exports.changePassword = async function (req, res, next) {
+  try {
+    const email = req.body.email;
+    let password = req.body.password;
+    let tokemEmailId;
+    let tokemPassword;
+    let newpassword=req.body.newpassword
+    const options = { new: true };
+  
+
+    const incryptPassword = await bcrypt.hash(req.body.newpassword, 10);
+    const updatedData={"password":incryptPassword};
+    if (req.headers && req.headers.authorization) {
+      const authorization = req.headers.authorization.split(' ')[1];
+     // console.log(authorization);
+      tokenModel.findOne({token: authorization}, function(err, user1){
+        if(err)return handleErr(err);
+         tokemEmailId=user1.email;
+         
+          //  console.log(tokemEmailId)
+          //  console.log(email)
+
+          if(tokemEmailId==email){
+            Model.findOne({email: email}, function(err, user2){
+              if(err)return handleErr(err);
+              //console.log(user2.password);
+              tokemPassword=user2.password;
+              const id=user2._id;
+              //console.log(id)
+              bcrypt.compare(req.body.password, tokemPassword, (err, result) => {
+                if (!result) {
+                  return res.status(401).json({
+                    msg: 'password matching failed',
+                  });
+                }
+                else{ 
+                  user2.password=incryptPassword;
+                  const asyncCall=async function () {
+                    const result = await  Model.findByIdAndUpdate(id, updatedData, options);
+                  }
+       
+                        
+                      asyncCall()
+                  
+                  console.log(user2.password);
+                  return res.status(401).json({
+                    msg: 'password changed',
+                  });}
+               
+                            })
+            })
+            }
+            else{
+              return res.status(401).json({
+                msg: 'wrong email id',
+              });
+            }
+            
+            
+        })
+      
+      
+    }
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
