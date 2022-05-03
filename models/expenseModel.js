6cde70c48c4332279e272bfeaf0bef7c6c70dc59
jaiswalshiva <jaiswal.shivam@tftus.com>
2022-05-03 11:08:24 +0530

    Add Second Argument

--- models/expenseModel.js
+++ models/expenseModel.js
@@ -2,28 +2,38 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userExpense = new Schema({
-  name: {
-    type: String,
-    required: true,
+const userExpense = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-  userID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-});
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model('Record', userExpense);
